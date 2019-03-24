b904631615b2e0823fb2b1a5ae91c22d0da2f662
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-24 16:18:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa << next_country
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1 + array2
+end
